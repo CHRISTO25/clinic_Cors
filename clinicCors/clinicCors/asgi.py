af3ad7206204bd12eac2e834dfa667de0c41ceea
CHRISTO25 <christovarghese2725@gmail.com>
2025-06-17 22:39:53 +0530

    again start

---
+++ clinicCors/clinicCors/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for clinicCors project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clinicCors.settings')
+
+application = get_asgi_application()
---
+++ clinicCors/prescriptionsApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PrescriptionsappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'prescriptionsApp'
