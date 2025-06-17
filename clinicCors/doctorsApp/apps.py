f59816766faad4d92eb00614bc6dbd730587e796
CHRISTO25 <christovarghese2725@gmail.com>
2025-06-17 21:58:31 +0530

    again

--- clinicCors/doctorsApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DoctorsappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'doctorsApp'
--- clinicCors/prescriptionsApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PrescriptionsappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'prescriptionsApp'
